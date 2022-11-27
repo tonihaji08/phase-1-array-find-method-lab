8559b8e1b669e38b69f17f70d5faf28a93770e94
Your Name <antony.adriano@stundent.moringaschool.com>
2022-11-27 16:29:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(record){
+    let win = record.find( record => record.result === "W")
+    if (win){
+        return win.year
+    }
+    else {
+        return undefined
+    }
+}
+
